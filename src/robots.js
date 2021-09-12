83187d5365fbf0c6963b4540f46f005a6b4f8a50
Junaidsyedd <89935169+Junaidsyedd@users.noreply.github.com>
2021-09-12 13:41:19 -0400

    resend

---
+++ src/robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Jiggy',
+	username: "Jiggy",
+	email:"Jiggy@gmail.com"
+},
+{
+	id:2,
+	name: "Jiggyta",
+	username: 'Jiggyta',
+	email: "Jiggyta@gmail.com"
+},
+{
+	id:3,
+	name: "Gizzy",
+	username: 'Gizzy',
+	email: "Gizzy@gmail.com"
+},
+{
+	id:4,
+	name: "Zazie",
+	username: 'Zazie',
+	email: "Zazie@gmail.com"
+},
+{
+	id:5,
+	name: "Batata",
+	username: 'Batata',
+	email: "Batata@gmail.com"
+},
+{
+	id:6,
+	name: "Jolie",
+	username: 'Jolie',
+	email: "Jolie@gmail.com"
+},
+{
+	id:7,
+	name: "Jignest",
+	username: 'Jignesh',
+	email: "Jignesh@gmail.com"
+},
+{
+	id:8,
+	name: "Rodger",
+	username: 'Rodger',
+	email: "Rodger@gmail.com"
+},
+{
+	id:9,
+	name: "Ailes",
+	username: 'Ailes',
+	email: "Ailes@gmail.com"
+},
+{
+	id:10,
+	name: "James",
+	username: 'James',
+	email: "James@gmail.com"
+},
+{
+	id:11,
+	name: "Charles",
+	username: 'Charles',
+	email: "Charles@gmail.com"
+},
+{
+	id:12,
+	name: "Dwayne",
+	username: 'Dwayne',
+	email: "Dwayne@gmail.com"
+}
+];
